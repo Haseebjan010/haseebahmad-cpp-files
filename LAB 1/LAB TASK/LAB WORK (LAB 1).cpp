55679d97dd8f2b12cce33a430dfb8134ffee363c
Haseebjan010 <ha4051482@gmail.com>
2026-03-10 23:46:03 +0500

    Add files via upload

---
+++ LAB 1/LAB TASK/LAB WORK (LAB 1).cpp
@@ -0,0 +1,17 @@
+// Name: Haseeb Ahmad
+// Department: Electrical Engineering (communication)
+// Registration No: BF25NWELE0678
+// Section: B
+
+
+
+#include <iostream>   // library ha iostream ma 
+using namespace std;  // standard name use krny k liay add kiay han
+
+int main() 
+{
+    
+    cout << "Hello World!"; // display k liye
+
+    return 0;  // program shuru b aur khatam b krta ha
+}
