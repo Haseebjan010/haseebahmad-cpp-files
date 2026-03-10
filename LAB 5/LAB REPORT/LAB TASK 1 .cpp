71653149d74752d187969593c463de69d817d958
Haseebjan010 <ha4051482@gmail.com>
2026-03-11 00:21:39 +0500

    Add files via upload

---
+++ LAB 5/LAB REPORT/LAB TASK 1 .cpp
@@ -0,0 +1,18 @@
+// Name: Haseeb Ahmad
+// Department: Electrical Engineering (Comm)
+// Registration No: BF25NWELE0678
+// Section: B
+#include <iostream>
+using namespace std;
+int main()
+{
+	int q;
+	do{
+		cout<<q<<endl;
+		q++;		
+	}
+	while(q<=10);
+	{
+		return 0;
+	}
+}
---
+++ LAB 5/LAB REPORT/LAB TASK 3 .cpp
@@ -0,0 +1,22 @@
+// Name: Haseeb Ahmad
+// Department: Electrical Engineering (Comm)
+// Registration No: BF25NWELE0678
+// Section: B
+#include <iostream>
+using namespace std;
+int main()
+{
+	int e; // int e decleare kiys ha user sa value lana ka liya
+	cout<<"enter positive integer : ";
+	cin>>e;
+	
+	int sum= 0; // sum karna ka liya 
+int q= 1;
+while(q<=e) //while loop ha  /*jab tak q ki value e ka equal nhi ho giya loop chalta raha ga       */
+
+{
+sum += q;
+q++;	
+}
+cout<<"sum from 1 to :"	<< e <<" is "<<sum; // output show kara ga
+}
